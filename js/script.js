53796507d2d1decf0262e3f6912030234114a9eb
Pablo <pablocavalcantee@gmail.com>
2023-08-04 09:17:35 -0300

    channges

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /* Typed */
  const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Design', 'Forte',],
+    strings: ['Frontend', 'React',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
